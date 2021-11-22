f8fb048ea6b6909e8fc755a2fa6b7d4baa2b12ec
onikd08 <onikd08@gmail.com>
2021-11-22 17:46:33 +0600

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    image = models.ImageField(
+        upload_to='uploads/images', null=False, blank=False)
+    name = models.CharField(max_length=150, null=False, blank=False)
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2, null=False, blank=False)
+    code = models.CharField(
+        max_length=10, null=False, blank=False)
+    description = models.TextField()
+    category = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.db.models import query
+from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+
+# Create your views here.
+
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
